18d9385b181a04757de856bbdd26b600e5cd0f0d
Patcharaphorn <59070117@it.kmitl.ac.th>
2017-11-11 15:04:21 +0000

    Update MainMenuScreen

--- core/src/com/ar/game/AnimalRacing.java
+++ core/src/com/ar/game/AnimalRacing.java
@@ -1,20 +1,17 @@
 package com.ar.game;
 
 import com.ar.game.handler.KeyboardController;
-import com.ar.game.screen.MainGameScreen;
-import com.ar.game.system.Systems;
+import com.ar.game.screen.MainMenuScreen;
 import com.badlogic.ashley.core.Engine;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.google.inject.Guice;
-import com.google.inject.Injector;
 
 public class AnimalRacing extends Game {
     public static String TITLE = "Animal Racing";
     public static int V_WIDTH = 640;
     public static int V_HEIGHT = 480;
     public static int SCALE = 2;
-    public Injector injector;
+
     public SpriteBatch batch;
     public Engine engine = new Engine();
     public static KeyboardController controller = new KeyboardController();
@@ -22,14 +19,7 @@ public class AnimalRacing extends Game {
 	@Override
 	public void create () {
 	    batch = new SpriteBatch();
-	    // Lines below need to run before MainGameScreen Starts
-	    if (true) { // If main game is going to begin| Change a condition to what ever u want
-            injector = Guice.createInjector(new GameModule(this));
-            injector.getInstance(Systems.class).list.stream()
-                    .map(systemClass -> injector.getInstance(systemClass))
-                    .forEach(entitySystem -> engine.addSystem(entitySystem));
-            this.setScreen(injector.getInstance(MainGameScreen.class));
-        }
+        this.setScreen(new MainMenuScreen(this));
 	}
 
 	@Override
