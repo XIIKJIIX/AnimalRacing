34cbbed85412a60bc82e5e25ecb54f470e82d564
Patcharaphorn <59070117@it.kmitl.ac.th>
2017-11-11 20:14:17 +0000

    Refactor code by using Scene2d instead

--- core/src/com/ar/game/screen/MainMenuScreen.java
+++ core/src/com/ar/game/screen/MainMenuScreen.java
@@ -8,6 +8,13 @@ import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.google.inject.Inject;
 
 import javax.swing.*;
@@ -19,17 +26,16 @@ public class MainMenuScreen extends ScreenAdapter {
     private Texture exitButtonInactive;
     private Texture playButtonActive;
     private Texture playButtonInactive;
+    private ImageButton playButton;
+    private ImageButton exitButton;
+
     private SpriteBatch batch;
     private AnimalRacing game;
-    private static final int EXIT_BUTTON_WIDTH = 300;
-    private static final int EXIT_BUTTON_HEIGHT = 100;
-    private static final int PLAY_BUTTON_WIDTH = 300;
-    private static final int PLAY_BUTTON_HEIGHT = 100;
-    private static final int EXIT_BUTTON_Y = 100;
-    private static final int PLAY_BUTTON_Y = 300;
+
 
     Object[] options = {"Yes", "No"};
 
+    private Stage stage;
 
     @Inject
     public MainMenuScreen (AssetManager manager, SpriteBatch batch, AnimalRacing game){
@@ -42,19 +48,38 @@ public class MainMenuScreen extends ScreenAdapter {
     }
 
     @Override
-    public void render(float delta) {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    public void show(){
 
-        batch.begin();
+        playButton = new ImageButton(new TextureRegionDrawable(new TextureRegion(playButtonInactive)));
+        playButton.setHeight(playButton.getHeight()/2);
+        playButton.setWidth(playButton.getWidth()/2);
+        playButton.setPosition(Gdx.graphics.getWidth()/2 - playButton.getWidth()/2, Gdx.graphics.getHeight()/2);
 
-        int x = AnimalRacing.V_WIDTH - EXIT_BUTTON_WIDTH/2;
-        if (Gdx.input.getX() < x + EXIT_BUTTON_WIDTH && Gdx.input.getX() > x &&
-                AnimalRacing.V_HEIGHT*2 - Gdx.input.getY() < EXIT_BUTTON_Y + EXIT_BUTTON_HEIGHT &&
-                AnimalRacing.V_HEIGHT*2 - Gdx.input.getY()> EXIT_BUTTON_Y) {
-            batch.draw(exitButtonActive, x, EXIT_BUTTON_Y, EXIT_BUTTON_WIDTH, EXIT_BUTTON_HEIGHT);
+        exitButton = new ImageButton(new TextureRegionDrawable(new TextureRegion(exitButtonInactive)));
+        exitButton.setHeight(exitButton.getHeight()/2);
+        exitButton.setWidth(exitButton.getWidth()/2);
+        exitButton.setPosition(Gdx.graphics.getWidth()/2 - exitButton.getWidth()/2, Gdx.graphics.getHeight()/4);
 
-            if (Gdx.input.isTouched()){
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+        playButton.addListener(new InputListener() {
+            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+                game.dispose();
+                game.injector.getInstance(Systems.class).list.stream()
+                        .map(systemClass -> game.injector.getInstance(systemClass))
+                        .forEach(entitySystem -> game.engine.addSystem(entitySystem));
+                game.setScreen(game.injector.getInstance(MainGameScreen.class));
+                return true;
+            }
+            public void touchDragged(InputEvent event, float x, float y, int pointer){}
+
+            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+
+            }
+        });
+
+        exitButton.addListener(new InputListener() {
+            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
                 UIManager.put("OptionPane.minimumSize",new Dimension(AnimalRacing.V_WIDTH,AnimalRacing.V_HEIGHT/2));
                 int n = JOptionPane.showOptionDialog(null,
                         "Do you want to exit the game?",
@@ -66,35 +91,32 @@ public class MainMenuScreen extends ScreenAdapter {
                 if (n == 0) {
                     Gdx.app.exit();
                 }
+                return true;
             }
-        }
 
-        else {
-            batch.draw(exitButtonInactive, x, EXIT_BUTTON_Y, EXIT_BUTTON_WIDTH, EXIT_BUTTON_HEIGHT);
-        }
+            public void touchDragged(InputEvent event, float x, float y, int pointer){}
 
-        if (Gdx.input.getX() < x + PLAY_BUTTON_WIDTH && Gdx.input.getX() > x &&
-                AnimalRacing.V_HEIGHT*2 - Gdx.input.getY() < PLAY_BUTTON_Y + PLAY_BUTTON_HEIGHT &&
-                AnimalRacing.V_HEIGHT*2 - Gdx.input.getY() > PLAY_BUTTON_Y){
-            batch.draw(playButtonActive, x, PLAY_BUTTON_Y, PLAY_BUTTON_WIDTH, PLAY_BUTTON_HEIGHT);
+            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
 
-            if (Gdx.input.isTouched()){
-                this.dispose();
-                game.injector.getInstance(Systems.class).list.stream()
-                        .map(systemClass -> game.injector.getInstance(systemClass))
-                        .forEach(entitySystem -> game.engine.addSystem(entitySystem));
-                game.setScreen(game.injector.getInstance(MainGameScreen.class));
             }
-        }
+        });
 
-        else {
-            batch.draw(playButtonInactive, x, PLAY_BUTTON_Y, PLAY_BUTTON_WIDTH, PLAY_BUTTON_HEIGHT);
-        }
+        stage.addActor(playButton);
+        stage.addActor(exitButton);
+    }
 
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
         batch.end();
     }
 
     @Override
     public void dispose() {
+        stage.dispose();
     }
 }
