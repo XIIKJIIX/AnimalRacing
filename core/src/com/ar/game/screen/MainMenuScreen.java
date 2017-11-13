4e4e492609b98fc630ce0ac6ef4fef8ba894713f
Patcharaphorn <59070117@it.kmitl.ac.th>
2017-11-13 08:27:19 +0000

    Add hover effects

--- core/src/com/ar/game/screen/MainMenuScreen.java
+++ core/src/com/ar/game/screen/MainMenuScreen.java
@@ -9,29 +9,30 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.google.inject.Inject;
-
 import javax.swing.*;
 import java.awt.*;
 
 public class MainMenuScreen extends ScreenAdapter {
 
-    private Texture exitButtonActive;
-    private Texture exitButtonInactive;
-    private Texture playButtonActive;
-    private Texture playButtonInactive;
-    private ImageButton playButton;
-    private ImageButton exitButton;
+    private Texture exitButtonActiveImage;
+    private Texture exitButtonInactiveImage;
+    private Texture playButtonActiveImage;
+    private Texture playButtonInactiveImage;
+    private ImageButton playButtonActive;
+    private ImageButton playButtonInactive;
+    private ImageButton exitButtonActive;
+    private ImageButton exitButtonInactive;
 
     private SpriteBatch batch;
     private AnimalRacing game;
-
 
     Object[] options = {"Yes", "No"};
 
@@ -41,28 +42,47 @@ public class MainMenuScreen extends ScreenAdapter {
     public MainMenuScreen (AssetManager manager, SpriteBatch batch, AnimalRacing game){
         this.batch = batch;
         this.game = game;
-        playButtonActive = manager.get("Play_active.jpg", Texture.class);
-        playButtonInactive = manager.get("Play_inactive.jpg", Texture.class);
-        exitButtonActive = manager.get("Exit_active.jpg", Texture.class);
-        exitButtonInactive = manager.get("Exit_inactive.jpg", Texture.class);
+        playButtonActiveImage = manager.get("Play_active.jpg", Texture.class);
+        playButtonInactiveImage = manager.get("Play_inactive.jpg", Texture.class);
+        exitButtonActiveImage = manager.get("Exit_active.jpg", Texture.class);
+        exitButtonInactiveImage = manager.get("Exit_inactive.jpg", Texture.class);
     }
+
+
 
     @Override
     public void show(){
 
-        playButton = new ImageButton(new TextureRegionDrawable(new TextureRegion(playButtonInactive)));
-        playButton.setHeight(playButton.getHeight()/2);
-        playButton.setWidth(playButton.getWidth()/2);
-        playButton.setPosition(Gdx.graphics.getWidth()/2 - playButton.getWidth()/2, Gdx.graphics.getHeight()/2);
+        playButtonInactive = new ImageButton(new TextureRegionDrawable(new TextureRegion(playButtonInactiveImage)));
+        playButtonInactive.setHeight(playButtonInactive.getHeight()/2);
+        playButtonInactive.setWidth(playButtonInactive.getWidth()/2);
+        playButtonInactive.setPosition(Gdx.graphics.getWidth()/2 - playButtonInactive.getWidth()/2, Gdx.graphics.getHeight()/2);
 
-        exitButton = new ImageButton(new TextureRegionDrawable(new TextureRegion(exitButtonInactive)));
-        exitButton.setHeight(exitButton.getHeight()/2);
-        exitButton.setWidth(exitButton.getWidth()/2);
-        exitButton.setPosition(Gdx.graphics.getWidth()/2 - exitButton.getWidth()/2, Gdx.graphics.getHeight()/4);
+        playButtonActive = new ImageButton(new TextureRegionDrawable(new TextureRegion(playButtonActiveImage)));
+        playButtonActive.setHeight(playButtonActive.getHeight()/2);
+        playButtonActive.setWidth(playButtonActive.getWidth()/2);
+        playButtonActive.setPosition(Gdx.graphics.getWidth()/2 - playButtonActive.getWidth()/2, Gdx.graphics.getHeight()/2);
+
+        exitButtonInactive = new ImageButton(new TextureRegionDrawable(new TextureRegion(exitButtonInactiveImage)));
+        exitButtonInactive.setHeight(exitButtonInactive.getHeight()/2);
+        exitButtonInactive.setWidth(exitButtonInactive.getWidth()/2);
+        exitButtonInactive.setPosition(Gdx.graphics.getWidth()/2 - exitButtonInactive.getWidth()/2, Gdx.graphics.getHeight()/4);
+
+        exitButtonActive = new ImageButton(new TextureRegionDrawable(new TextureRegion(exitButtonActiveImage)));
+        exitButtonActive.setHeight(exitButtonActive.getHeight()/2);
+        exitButtonActive.setWidth(exitButtonActive.getWidth()/2);
+        exitButtonActive.setPosition(Gdx.graphics.getWidth()/2 - exitButtonActive.getWidth()/2, Gdx.graphics.getHeight()/4);
 
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
-        playButton.addListener(new InputListener() {
+
+        playButtonInactive.addListener(new ClickListener() {
+            public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor){
+                stage.addActor(playButtonActive);
+            }
+            public void exit(InputEvent event, float x, float y, int pointer, Actor toActor){
+                playButtonActive.remove();
+            }
             public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
                 game.dispose();
                 game.injector.getInstance(Systems.class).list.stream()
@@ -71,14 +91,32 @@ public class MainMenuScreen extends ScreenAdapter {
                 game.setScreen(game.injector.getInstance(MainGameScreen.class));
                 return true;
             }
-            public void touchDragged(InputEvent event, float x, float y, int pointer){}
+        });
 
-            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
-
+        playButtonActive.addListener(new ClickListener() {
+            public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor){
+                playButtonActive.remove();
+            }
+            public void exit(InputEvent event, float x, float y, int pointer, Actor toActor){
+                stage.addActor(playButtonInactive);
+            }
+            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+                game.dispose();
+                game.injector.getInstance(Systems.class).list.stream()
+                        .map(systemClass -> game.injector.getInstance(systemClass))
+                        .forEach(entitySystem -> game.engine.addSystem(entitySystem));
+                game.setScreen(game.injector.getInstance(MainGameScreen.class));
+                return true;
             }
         });
 
-        exitButton.addListener(new InputListener() {
+        exitButtonInactive.addListener(new ClickListener() {
+            public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor){
+                stage.addActor(exitButtonActive);
+            }
+            public void exit(InputEvent event, float x, float y, int pointer, Actor toActor){
+                exitButtonActive.remove();
+            }
             public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
                 UIManager.put("OptionPane.minimumSize",new Dimension(AnimalRacing.V_WIDTH,AnimalRacing.V_HEIGHT/2));
                 int n = JOptionPane.showOptionDialog(null,
@@ -93,16 +131,34 @@ public class MainMenuScreen extends ScreenAdapter {
                 }
                 return true;
             }
+        });
 
-            public void touchDragged(InputEvent event, float x, float y, int pointer){}
-
-            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
-
+        exitButtonActive.addListener(new ClickListener() {
+            public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor){
+                exitButtonActive.remove();
+            }
+            public void exit(InputEvent event, float x, float y, int pointer, Actor toActor){
+                stage.addActor(exitButtonInactive);
+            }
+            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+                UIManager.put("OptionPane.minimumSize",new Dimension(AnimalRacing.V_WIDTH,AnimalRacing.V_HEIGHT/2));
+                int n = JOptionPane.showOptionDialog(null,
+                        "Do you want to exit the game?",
+                        "Warning",
+                        JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE,
+                        null,
+                        options,
+                        options);
+                if (n == 0) {
+                    Gdx.app.exit();
+                }
+                return true;
             }
         });
 
-        stage.addActor(playButton);
-        stage.addActor(exitButton);
+        stage.addActor(playButtonInactive);
+        stage.addActor(exitButtonInactive);
+
     }
 
     @Override
@@ -110,7 +166,7 @@ public class MainMenuScreen extends ScreenAdapter {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        stage.act(Gdx.graphics.getDeltaTime());
+        stage.act();
         stage.draw();
         batch.end();
     }
