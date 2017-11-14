ec7aa710aea0428df102aabe8b6ea151cf89c427
Weeruhputt Supsohmboon <59070162@it.kmitl.ac.th>
2017-11-14 19:58:23 +0700

    Use Enum as State

--- core/src/com/ar/game/component/StateComponent.java
+++ core/src/com/ar/game/component/StateComponent.java
@@ -3,22 +3,18 @@ package com.ar.game.component;
 import com.badlogic.ashley.core.Component;
 
 public class StateComponent implements Component {
-    public static final int STATE_NORMAL = 0;
-    public static final int STATE_JUMPING = 1;
-    public static final int STATE_FALLING = 2;
-    public static final int STATE_MOVING = 3;
-    public static final int STATE_HIT = 4;
-
-    private int state = 0;
+    public static enum State {NORMAL, JUMPING, FALLING, MOVIING, HIT};
+    public State state = State.NORMAL;
+    public State prevState = State.NORMAL;
     public float time = 0.0f;
     public boolean isLooping = false;
 
-    public void set(int newState) {
+    public void set(State newState) {
         state = newState;
         time = 0.0f;
     }
 
-    public int get() {
+    public State get() {
         return state;
     }
 }
