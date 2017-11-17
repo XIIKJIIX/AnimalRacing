69b6a77609c856803d4ea82a7011a1d596d5c544
Weeruhputt Supsohmboon <59070162@it.kmitl.ac.th>
2017-11-17 11:59:07 +0700

    Create bomb

--- core/src/com/ar/game/entity/Bomb.java
+++ core/src/com/ar/game/entity/Bomb.java
@@ -2,21 +2,12 @@ package com.ar.game.entity;
 
 import com.ar.game.component.*;
 import com.badlogic.ashley.core.Entity;
-import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
-import com.badlogic.gdx.utils.Array;
-import com.google.inject.Inject;
-
-import java.util.HashMap;
 
 import static com.ar.game.constant.B2Dvars.PPM;
 
-
-public class IceBall extends Entity implements Skill {
-
+public class Bomb extends Entity implements Skill {
     public TransformComponent transform;
     public PhysicsComponent physics;
     public DataComponent data;
@@ -26,13 +17,12 @@ public class IceBall extends Entity implements Skill {
     public AnimationComponent animationComponent;
     public Body body;
 
-    public IceBall(World world) {
-        skill = new SkillComponent(SkillComponent.ShotType.BULLET, SkillComponent.ON_HIT.GONE, 10f, 5f, 0.25f);
-        skill.addSkill(SkillComponent.Type.DAMAGE, 15f);
-        skill.addSkill(SkillComponent.Type.SLOW, 0.35f);
+    public Bomb(World world) {
+        skill = new SkillComponent(SkillComponent.ShotType.PROJECTILE, SkillComponent.ON_HIT.GONE, 12f, 5f, 0.25f);
+        skill.addSkill(SkillComponent.Type.DAMAGE, 75f);
         add(skill);
 
-        data = new DataComponent(5f / PPM, 5f / PPM, "ICE_BALL");
+        data = new DataComponent(15f / PPM, 15f / PPM, "ICE_BALL");
         add(data);
 
         type = new TypeComponent(TypeComponent.SKILL);
@@ -52,24 +42,9 @@ public class IceBall extends Entity implements Skill {
         body.setUserData(this);
 
         add(new CollisionComponent());
-
-        TextureAtlas atlas = new TextureAtlas("skills/ice_ball.txt");
-        Array<TextureRegion> frames = new Array<>();
-        for (int i = 0; i < 6; i++) {
-            frames.add(new TextureRegion(atlas.findRegion((i+1)+""), 0, 0, 512, 197));
-        }
-        Animation animation = new Animation(0.1f, frames);
-        frames.clear();
-        HashMap<StateComponent.State, Animation> stateAnimation = new HashMap<>();
-        stateAnimation.put(StateComponent.State.MOVING, animation);
-        animationComponent = new AnimationComponent(stateAnimation);
-        add(animationComponent);
-
-        state = new StateComponent();
-        state.set(StateComponent.State.MOVING);
-        add(state);
     }
 
+    @Override
     public void setTransform(Vector2 position) {
         transform = new TransformComponent(position);
         body.setTransform(transform.position, 0);
@@ -80,16 +55,11 @@ public class IceBall extends Entity implements Skill {
 
     @Override
     public Body getBody() {
-        return body;
+        return physics.body;
     }
 
     @Override
     public float getCooldown() {
         return skill.cooldown;
-    }
-
-    @Override
-    public String toString() {
-        return "ICEBALL";
     }
 }
