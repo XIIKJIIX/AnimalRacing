974d4d572887d78d22d14af49d45ea2ce8e25148
Weeruhputt Supsohmboon <59070162@it.kmitl.ac.th>
2017-11-19 18:00:59 +0700

    Try Endscreen

--- core/src/com/ar/game/system/DyingSystem.java
+++ core/src/com/ar/game/system/DyingSystem.java
@@ -1,6 +1,8 @@
 package com.ar.game.system;
 
+import com.ar.game.AnimalRacing;
 import com.ar.game.component.*;
+import com.ar.game.screen.EndScreen;
 import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
@@ -11,20 +13,24 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.physics.box2d.World;
 import com.google.inject.Inject;
 
+import java.util.stream.Stream;
+
 public class DyingSystem extends IteratingSystem {
 
     private OrthographicCamera camera;
     private World world;
     private Engine engine;
     private  SpriteBatch batch;
+    private AnimalRacing game;
 
     @Inject
-    public DyingSystem( OrthographicCamera camera, World world, Engine engine, SpriteBatch batch) {
+    public DyingSystem( OrthographicCamera camera, World world, Engine engine, SpriteBatch batch, AnimalRacing game) {
         super(Family.all(PlayerComponent.class).get());
         this.camera = camera;
         this.world = world;
         this.engine = engine;
         this.batch = batch;
+        this.game = game;
     }
 
     @Override
@@ -39,6 +45,10 @@ public class DyingSystem extends IteratingSystem {
         if (x <= (camera.position.x - camera.viewportWidth / 2) || y <= (camera.position.y - camera.viewportHeight / 2) || player.health <= 0) {
             Gdx.app.log(data.name + " Status", "Died");
             SystemHelper.removeEntity(entity, world, engine);
+            engine.removeAllEntities();
+            System.out.println(engine.getEntities().size());
+            game.dispose();
+            game.setScreen(AnimalRacing.injector.getInstance(EndScreen.class));
         }
     }
 
