2cedbf1780064d5895c586ea390bba9968a103fe
Patcharaphorn <59070117@it.kmitl.ac.th>
2017-11-19 13:28:50 +0000

    Add icon

--- desktop/src/com/ar/game/desktop/DesktopLauncher.java
+++ desktop/src/com/ar/game/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.ar.game.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.ar.game.AnimalRacing;
@@ -10,6 +11,9 @@ public class DesktopLauncher {
 		config.title = AnimalRacing.TITLE;
 		config.width = AnimalRacing.V_WIDTH * AnimalRacing.SCALE;
 		config.height = AnimalRacing.V_HEIGHT * AnimalRacing.SCALE;
+		config.addIcon("icon(16x16).png", Files.FileType.Local);
+		config.addIcon("icon(32x32).png", Files.FileType.Local);
+		config.addIcon("icon(128x128).png", Files.FileType.Local);
 		new LwjglApplication(new AnimalRacing(), config);
 	}
 }
